2a1d0a0cead817cd920725bd34e966b82e7e38c6
Dilan Asiri <dilanasiri95@gmail.com>
2023-08-29 13:34:37 +0530

    Adding Food Details To The Cards

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -1,21 +1,102 @@
 import { Injectable } from '@angular/core';
-
+import { Food } from 'src/app/shared/models/Food';
 @Injectable({
   providedIn: 'root'
 })
 export class FoodService {
 
   constructor() { }
-  getAll():string[]{
+  getAll():Food[]{
     return[
-      'assets/images/foods/1.jpg',
-      'assets/images/foods/2.jpg',
-      'assets/images/foods/3.png',
-      'assets/images/foods/4.jpeg',
-      'assets/images/foods/5.webp',
-      'assets/images/foods/6.jpg',
-      'assets/images/foods/7.jpg',
-      'assets/images/foods/8.jpg',
+      {
+        id:1,
+        name:'burger',
+        price:250,
+        cookTime:'40-50',
+        favorite:false,
+        origins:['italy'],
+        stars: 4.0,
+        imageUrl: '/asserts/images/food/1.jpg',
+        tags:['FastFood','Pizza','Lunch'],
+      },
+      {
+        id:2,
+        name:'burger',
+        price:250,
+        cookTime:'40-50',
+        favorite:false,
+        origins:['italy'],
+        stars: 4.0,
+        imageUrl: '/asserts/images/food/2.jpg',
+        tags:['FastFood','Pizza','Lunch'],
+      },
+      {
+        id:3,
+        name:'burger',
+        price:250,
+        cookTime:'40-50',
+        favorite:false,
+        origins:['italy'],
+        stars: 4.0,
+        imageUrl: '/asserts/images/food/3.jpg',
+        tags:['FastFood','Pizza','Lunch'],
+      },
+      
+      {
+        id:4,
+        name:'burger',
+        price:250,
+        cookTime:'40-50',
+        favorite:false,
+        origins:['italy'],
+        stars: 4.0,
+        imageUrl: '/asserts/images/food/4.jpg',
+        tags:['FastFood','Pizza','Lunch'],
+      },
+      {
+        id:5,
+        name:'burger',
+        price:250,
+        cookTime:'40-50',
+        favorite:false,
+        origins:['italy'],
+        stars: 4.0,
+        imageUrl: '/asserts/images/food/5.jpg',
+        tags:['FastFood','Pizza','Lunch'],
+      },
+      {
+        id:6,
+        name:'burger',
+        price:250,
+        cookTime:'40-50',
+        favorite:false,
+        origins:['italy'],
+        stars: 4.0,
+        imageUrl: '/asserts/images/food/6.jpg',
+        tags:['FastFood','Pizza','Lunch'],
+      },
+      {
+        id:7,
+        name:'burger',
+        price:250,
+        cookTime:'40-50',
+        favorite:false,
+        origins:['italy'],
+        stars: 4.0,
+        imageUrl: '/asserts/images/food/7.jpg',
+        tags:['FastFood','Pizza','Lunch'],
+      },
+      {
+        id:8,
+        name:'burger',
+        price:250,
+        cookTime:'40-50',
+        favorite:false,
+        origins:['italy'],
+        stars: 4.0,
+        imageUrl: '/asserts/images/food/8.jpg',
+        tags:['FastFood','Pizza','Lunch'],
+      },
     ]
   }
 }
